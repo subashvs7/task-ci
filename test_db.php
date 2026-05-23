86a106f41e9c86ffdada325253ac398f56d7d866
kaviyarasan <mkaviyarasan003@gmail.com>
2026-05-23 15:52:01 +0530

    new update

--- test_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('BASEPATH', '1');
-require 'application/config/database.php';
-$conn = new mysqli('127.0.0.1', $db['default']['username'], $db['default']['password'], $db['default']['database']);
-$r = $conn->query("DESCRIBE tm_tasks");
-while($row = $r->fetch_assoc()) { print_r($row); }
